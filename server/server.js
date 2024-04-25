a57ce6dbc6e9cd288276f7dce47d5dfd5d65d912
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-04-25 14:13:11 -0700

    boilerplate

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const express = require("express");
+const routes = require("./routes");
+
+require("dotenv").config();
+const uri = process.env.MONGODB_URL;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-mongoose.connect('mongodb://localhost/mern-stack', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
