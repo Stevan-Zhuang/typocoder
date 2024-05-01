f29de0ea78811770f8f9767d088ab01538a115fb
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-04-30 18:00:32 -0700

    feat: add server endpoint to query currently logged in user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,8 +11,14 @@ router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
   function(req, res) {
+    req.session.user = req.user;
+    res.cookie('session_id', req.session.id);
     res.redirect(process.env.FRONTEND_URL);
   },
 );
+
+router.get('/session', (req, res) => {
+  res.json({ user: req.session.user });
+});
 
 module.exports = router;
