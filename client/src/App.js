099b569bcb80887d5487169d5c8aa56e6ee38fe2
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-04-30 18:02:59 -0700

    feat: refactor components into global App.js and add UserContext

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import TypingGame from "./TypingGame";
+import SignInButton from "./SignInButton";
+import UserDisplay from "./UserDisplay";
+import { UserContext } from "./UserContext";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/session")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data.user);
+      })
+      .catch((error) => {
+        setUser(null);
+      });
+  }, []);
+
+  return (
+    <UserContext.Provider value={user}>
+      <TypingGame />
+      <SignInButton />
+      <UserDisplay />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
