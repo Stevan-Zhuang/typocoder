128dcd6b782a8ab3162d40e6638db74c705f85bc
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-05-01 18:24:13 -0700

    chore: format TextDisplay.js

--- client/src/TextDisplay.js
+++ client/src/TextDisplay.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function TextDisplay({ lines }) {
-  const codeString = lines.join('\n');
+  const codeString = lines.join("\n");
   return (
     <div style={{ fontFamily: "monospace" }}>
-      <SyntaxHighlighter language="javascript" style={solarizedlight} showLineNumbers={true}>
+      <SyntaxHighlighter
+        language="javascript"
+        style={solarizedlight}
+        showLineNumbers={true}
+      >
         {codeString}
       </SyntaxHighlighter>
     </div>
