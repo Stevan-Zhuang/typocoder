2a6b40ab475c11f69946f09bdbde0c3d037ce33c
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-05-02 20:16:55 -0700

    feat: make theme and language settings required in schema

--- server/models/UserSettings.js
+++ server/models/UserSettings.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const UserSettingsSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
   theme: {
     type: String,
-    default: 'light'
+    default: "light",
+    required: true,
   },
   language: {
     type: String,
-    default: 'en'
-  }
+    default: "en",
+    required: true,
+  },
 });
 
-const UserSettings = model('UserSettings', UserSettingsSchema);
+const UserSettings = model("UserSettings", UserSettingsSchema);
 module.exports = UserSettings;
