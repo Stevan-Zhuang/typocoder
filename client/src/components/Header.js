50c762bbfbef4bb58a00e326df434d0b24b7c316
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-05-02 22:33:06 -0700

    feat: change look of header and add temp css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,12 +2,13 @@ import React from "react";
 import SignInButton from "./SignInButton";
 import SignOutButton from "./SignOutButton";
 import UserDisplay from "./UserDisplay";
+import '../styles/Header.css';
 
 function Header({ user }) {
   return (
-    <header>
-      {user ? <SignOutButton /> : <SignInButton />}
+    <header className="header">
       <UserDisplay />
+      {user ? <SignOutButton /> : <SignInButton />}
     </header>
   );
 }
