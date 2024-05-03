a60f70bcc6efa7b1422ab41354061574f994208c
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-05-02 22:23:38 -0700

    fix: not showing name when logged in

--- client/src/components/UserDisplay.js
+++ client/src/components/UserDisplay.js
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../contexts/UserContext';
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 function UserDisplay() {
   const user = useContext(UserContext);
 
-  if (!user) {
-    return <div>{user ? user.name : "Guest"}</div>;
-  }
+  return (
+    <div>
+      <h2>{user ? user.name : "Guest"}</h2>
+    </div>
+  );
 }
 
 export default UserDisplay;
