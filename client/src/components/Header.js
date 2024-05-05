6b238b2d1ecbffcb747c69d37debba8623100430
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-05-05 13:24:36 -0700

    feat: add site title that reloads page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,8 +6,12 @@ import SettingsPopup from "./SettingsPopup";
 import "../styles/Header.css";
 
 function Header({ user, setSettings }) {
+  const reloadPage = () => {
+    window.location.reload();
+  };
   return (
     <header className="header">
+      <button onClick={reloadPage} className="siteTitle">TypoCoder</button>
       <SettingsPopup setSettings={setSettings} />
       <div className="userProfile">
         <UserDisplay user={user} />
