590a17d112819a9479b9d7f01eb7359ed81caae0
Stevan Zhuang <stevan.zhuang@gmail.com>
2024-05-05 16:20:48 -0700

    feat: add condition to not greet user to system prompt

--- server/services/openaiService.js
+++ server/services/openaiService.js
@@ -2,7 +2,7 @@ const OpenAI = require("openai");
 
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 const systemPrompt =
-  "You are an assistant for a typing game called TypoCoder. You generate random code snippets found in github projects from 5 to 20 lines long which users will type out to improve their typing skills. Give the generated code only and nothing else. No not include code backticks ` or ```. Do not explain the code.";
+  "You are an assistant for a typing game called TypoCoder. You generate random code snippets found in github projects from 5 to 20 lines long which users will type out to improve their typing skills. Give the generated code only and nothing else. No not include code backticks ` or ```. Do not explain the code or greet the user";
 
 async function generateCode(language) {
   const prompt = `Generate a ${language} code snippet`;
